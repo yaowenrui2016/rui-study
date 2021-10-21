d66f180dc57d82ddaea3dd79ad83f7821392b863
姚文锐 <yaowr@landray.com.cn>
2021-10-21 21:22:39 +0800

    发送待办请求

--- study-something/src/main/java/indi/rui/study/something/REST接口调用案例/SequentialSendAndDone.java
+++ study-something/src/main/java/indi/rui/study/something/REST接口调用案例/SequentialSendAndDone.java
@@ -12,10 +12,10 @@ import java.util.Objects;
 
 /**
  * @author: yaowr
- * @create: 2021-10-20
+ * @create: 2021-10-21
  */
 @Slf4j
-public class SendNotify {
+public class SequentialSendAndDone {
 
     private static final String ADDRESS = "http://localhost:8040";
     private static final String X_SERVICE_NAME = "73456775666d4c416f73776139584a4131432f6847413d3d";
@@ -24,49 +24,19 @@ public class SendNotify {
     private static final String DONE_TODO_JSON_PATH = "json/done_todo.json";
 
     public static void main(String[] args) {
-        SendNotify sendNotify = new SendNotify();
-        for (int i = 100; i < 200; i++) {
-            sendNotify.send(i + "");
-            sendNotify.done(i + "");
-            sendNotify.removeTodo(i + "");
-            sendNotify.removeDone(i + "");
-            sendNotify.removeAll(i + "");
-            sendNotify.suspend(i + "");
-            sendNotify.resume(i + "");
-            sendNotify.read(i + "");
+        SequentialSendAndDone seq = new SequentialSendAndDone();
+        for (int i = 2000; i < 3000; i++) {
+            seq.sendTodo(i + "");
+            seq.doneTodo(i + "");
         }
     }
 
-    private void send(String entityId) {
+    private void sendTodo(String entityId) {
         call("send", SEND_TODO_JSON_PATH, entityId);
     }
 
-    private void done(String entityId) {
+    private void doneTodo(String entityId) {
         call("done", DONE_TODO_JSON_PATH, entityId);
-    }
-
-    private void removeTodo(String entityId) {
-        call("removeTodo", DONE_TODO_JSON_PATH, entityId);
-    }
-
-    private void removeDone(String entityId) {
-        call("removeDone", DONE_TODO_JSON_PATH, entityId);
-    }
-
-    private void removeAll(String entityId) {
-        call("removeAll", DONE_TODO_JSON_PATH, entityId);
-    }
-
-    private void suspend(String entityId) {
-        call("suspend", DONE_TODO_JSON_PATH, entityId);
-    }
-
-    private void resume(String entityId) {
-        call("resume", DONE_TODO_JSON_PATH, entityId);
-    }
-
-    private void read(String entityId) {
-        call("read", DONE_TODO_JSON_PATH, entityId);
     }
 
     private void call(String method, String bodyPath, String entityId) {
@@ -80,9 +50,7 @@ public class SendNotify {
         Map<String, String> header = Collections.singletonMap("x-service-name", X_SERVICE_NAME);
         try {
             String response = HttpClientUtils.httpPost(sendUrl, body, header);
-            if (response.contains("\"success\":false")){
-                log.info("{} request: {} >>> response: {}", method, JSONUtils.toJSONString(body), response);
-            }
+            log.info("{} request: {} >>> response: {}", method, JSONUtils.toJSONString(body), response);
         } catch (Exception e) {
             log.error("{} exception", method, e);
         }
