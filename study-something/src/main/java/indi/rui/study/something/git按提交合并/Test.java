5960a731d4e5f42542e3ed1abd9fa8a877e23fe3
yaowr <yaowr@landray.com.cn>
2021-12-18 10:57:20 +0800

    git合并测试-提交a

---
+++ study-something/src/main/java/indi/rui/study/something/git按提交合并/Test.java
@@ -0,0 +1,15 @@
+package indi.rui.study.something.git按提交合并;
+
+/**
+ * @author: yaowr
+ * @create: 2021-12-18
+ */
+public class Test {
+    public static void main(String[] args) {
+        a();
+    }
+
+    private static void a() {
+
+    }
+}
