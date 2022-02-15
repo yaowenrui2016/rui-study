e1e588a56c8bc7b880f8f8670f7ce61dc42cf1a0
yaowr <yaowr@landray.com.cn>
2022-02-15 14:28:05 +0800

    增加react-server

---
+++ study-microservice/study-react-server/src/main/java/indi/rui/study/react/ReactServer.java
@@ -0,0 +1,15 @@
+package indi.rui.study.react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: yaowr
+ * @create: 2022-02-15
+ */
+@SpringBootApplication
+public class ReactServer {
+    public static void main(String[] args) {
+        SpringApplication.run(ReactServer.class, args);
+    }
+}
---
+++ study-microservice/study-react-server/src/main/resources/static/precache-manifest.90e8fda75258cab17a08d0dc59765f9a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c949c07068c8ef5d1b35a63688db86",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1ffaa38e382a6ee20e51",
+    "url": "./static/css/2.eb365711.chunk.css"
+  },
+  {
+    "revision": "2994c6a709e2f49a7d3e",
+    "url": "./static/css/main.2958ff2f.chunk.css"
+  },
+  {
+    "revision": "1ffaa38e382a6ee20e51",
+    "url": "./static/js/2.bc84fdc8.chunk.js"
+  },
+  {
+    "revision": "2994c6a709e2f49a7d3e",
+    "url": "./static/js/main.0a90e867.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "ffe9256f6f1fb657eee2a3fc7b55d45d",
+    "url": "./static/media/Logo.ffe9256f.svg"
+  },
+  {
+    "revision": "c155b94e96be2c08d330ca81c9d6a485",
+    "url": "./static/media/login_3.c155b94e.jpg"
+  }
+]);
