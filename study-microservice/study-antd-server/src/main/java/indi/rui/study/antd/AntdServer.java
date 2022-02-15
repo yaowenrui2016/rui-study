512f3029529d1191331dcf45f233f79fdf2f3695
yaowr <yaowr@landray.com.cn>
2022-02-15 18:18:06 +0800

    增加antd-server

---
+++ study-microservice/study-antd-server/src/main/java/indi/rui/study/antd/AntdServer.java
@@ -0,0 +1,15 @@
+package indi.rui.study.antd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: yaowr
+ * @create: 2022-02-15
+ */
+@SpringBootApplication
+public class AntdServer {
+    public static void main(String[] args) {
+        SpringApplication.run(AntdServer.class, args);
+    }
+}
